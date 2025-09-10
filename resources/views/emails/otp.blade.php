27d86f21e3bf1eb7e6092dd6918df7ee2e7035dd
abed-obaah <ubahobaah@gmail.com>
2025-09-10 17:37:25 +0100

    prod

--- resources/views/emails/otp.blade.php
+++ resources/views/emails/otp.blade.php
@@ -8,7 +8,7 @@
     <p>Hello {{ $user->name }},</p>
     <p>Your OTP verification code is: <strong>{{ $otp }}</strong></p>
     <p>This code will expire in 10 minutes.</p>
-    <p>If you didn't request this code, pleases ignore this email.</p>
+    <p>If you didn't request this code, please ignore this email.</p>
     <br>
     <p>Thank you,<br>{{ config('app.name') }}</p>
 </body>
